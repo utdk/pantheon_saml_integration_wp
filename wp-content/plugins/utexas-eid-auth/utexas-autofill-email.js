09b6fa94b790255e000a48e15ae7abeda5a88592
jmf3658 <jfullmer@austin.utexas.edu>
2023-07-06 13:54:24 -0500

    Remove wp-content directory

--- wp-content/plugins/utexas-eid-auth/utexas-autofill-email.js
+++
@@ -1,9 +0,0 @@
-jQuery( function() {
-  jQuery("#user_login").blur(function(e) {
-    var username = jQuery(this).val();
-    if (username) {
-      jQuery("#email").val( username + "@eid.utexas.edu");
-    }
-  });
-});
-
